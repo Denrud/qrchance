0a5417b30b68cc80f6724f24139b5bc57c26b536
DRUD <rudikdenis@gmail.com>
2020-06-29 01:22:42 +0300

    add second bloc

--- resources/views/home_page.blade.php
+++ resources/views/home_page.blade.php
@@ -43,14 +43,20 @@
 
     <main role="main">
 
-      <section class="main-banner">
+      <section class="main-banner-bg">
         <div class="container">
-          <h1 class="jumbotron-heading">Album example</h1>
-          <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
-          <p>
-            <a href="#" class="btn btn-primary my-2">Main call to action</a>
-            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-          </p>
+          <div class="col-6">
+
+            <h1 class="jumbotron-heading">Album example</h1>
+              <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+              <p>
+                <a href="#" class="btn btn-primary my-2">Main call to action</a>
+                <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+              </p>
+          </div>
+        </div>
+        <div class="col-6">
+          <span></span>
         </div>
       </section>
 
